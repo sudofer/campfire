4eb777ea04cc6b60a8eededa30f5a8cdb775e2de
Vivian Cheung <hi@viviancheung.ca>
2021-08-02 15:23:06 -0400

    implement websockets and add join room functionality

--- campfire-server/index.js
+++ campfire-server/index.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const users = require ('./routes/users');
-var cors = require('cors');
+const cors = require('cors');
+const socketio = require('socket.io');
 const app = express();
 app.use(cors());
 app.use('/users', users)
+
+const PORT = process.env.PORT || 3002;
+
 const http = require('http');
 const server = http.createServer(app);
-
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", socket => {
-  console.log("back end connected")
-  socket.send('hello');
-  socket.onmessage = event => {
-    console.log(`Message Received: ${event.data}`);
-
-    if (event.data === "ping") {
-      socket.send(JSON.stringify("pong"));
-    }
-  };
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
-const PORT = 3002;
+io.on("connection", socket => {
+  console.log("back end connected")
+  socket.send('hello');
+  
+  socket.on("createRoom", ({roomID}) => {
+    console.log(roomID)
+    socket.join(roomID)
+    io.to(roomID).emit('joinedRoom')
+  });
+});
+
 
 server.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
