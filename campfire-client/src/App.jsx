cffcc9f2a4e86a5c917aab56fec10405bbe2dc13
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-08-02 17:17:02 -0400

    not sure anymore ngl

--- campfire-client/src/App.jsx
+++ campfire-client/src/App.jsx
@@ -1,46 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { io } from "socket.io-client";
 import Home from "../src/components/home/Home";
 import Navbar from "./components/navbar/Navbar";
 
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Switch
-} from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 class App extends Component {
-  state = {users: []}
+  state = { users: [] };
 
   componentDidMount() {
-    fetch('http://localhost:3002/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  
-      this.socket = io ('ws://localhost:3002');
-      this.socket.on('joinedRoom', () => {
-        console.log("successfully joined room")
-      })
+    fetch("http://localhost:3002/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ users }));
+
+    this.socket = io("ws://localhost:3002");
+    this.socket.on("joinedRoom", () => {
+      console.log("successfully joined room");
+    });
 
     //   this.socket.onmessage = event => {
     //   const data = JSON.parse(event.data);
     //     console.log("this is my data:", data)
     // };
-  
-    }
+  }
 
-    ID = function () {   
-      // Math.random should be unique because of its seeding algorithm.   
-      // Convert it to base 36 (numbers + letters), and grab the first 9 characters   // after the decimal.   
-      return '_' + Math.random().toString(36).substr(2, 9); 
-    };
+  ID = function () {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters   // after the decimal.
+    return "_" + Math.random().toString(36).substr(2, 9);
+  };
 
   render() {
-    console.log(`${JSON.stringify(this.state)}`)
+    console.log(`${JSON.stringify(this.state)}`);
     return (
       <>
-      <Router>
+        {/* <Router>
         <div className="App">
           <Navbar />
           <Route path="/">
@@ -50,22 +44,23 @@ class App extends Component {
             <h1>About</h1>
           </Route>
         </div>
-      </Router>
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
+      </Router> */}
+        <div className="App">
+          <h1>Users</h1>
+          {this.state.users.map((user) => (
+            <div key={user.id}>{user.username}</div>
+          ))}
+        </div>
 
-      <button onClick={() => {
-        const roomID = this.ID();
-        console.log(roomID)
-        this.socket.emit("createRoom", {roomID})
-
-      }}>Create room</button>
-
-
+        <button
+          onClick={() => {
+            const roomID = this.ID();
+            console.log(roomID);
+            this.socket.emit("createRoom", { roomID });
+          }}
+        >
+          Create room
+        </button>
       </>
     );
   }
