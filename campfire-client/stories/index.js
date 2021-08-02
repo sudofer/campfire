99f78ad37260da120d688b5cc9705762a4817960
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-08-02 12:23:14 -0400

    materialui for storybook

--- campfire-client/stories/index.js
+++ campfire-client/stories/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { muiTheme } from "storybook-addon-material-ui";
 import Home from "components/home/Home";
+import theme from "../src/theme";
 
-storiesOf("Home", module).add("Home", () => <Home />);
+storiesOf("Home", module)
+  .addDecorator(muiTheme([theme]))
+  .add("Home", () => <Home />);
