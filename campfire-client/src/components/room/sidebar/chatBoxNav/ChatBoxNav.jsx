d582c8d5e1ea1505eb33f7e2ee69512eeb917006
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-08-02 15:20:03 -0400

    component stories

---
+++ campfire-client/src/components/room/sidebar/chatBoxNav/ChatBoxNav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Paper, Tabs, Tab } from "@material-ui/core";
+export default function ChatBoxNav() {
+  const [value, setValue] = React.useState(2);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper square>
+      <Tabs
+        value={value}
+        indicatorColor="secondary"
+        textColor="secondary"
+        onChange={handleChange}
+      >
+        <Tab label="Chat" />
+        <Tab label="Playlist" />
+        <Tab label="Search" />
+      </Tabs>
+    </Paper>
+  );
+}
--- campfire-client/stories/index.js
+++ campfire-client/stories/index.js
@@ -3,7 +3,12 @@ import { storiesOf } from "@storybook/react";
 import { muiTheme } from "storybook-addon-material-ui";
 import Home from "components/home/Home";
 import theme from "../src/theme";
+import ChatBoxNav from "../src/components/room/sidebar/chatBoxNav/ChatBoxNav";
 
 storiesOf("Home", module)
   .addDecorator(muiTheme([theme]))
   .add("Home", () => <Home />);
+
+storiesOf("ChatBoxNav", module)
+  .addDecorator(muiTheme([theme]))
+  .add("ChatBoxNav", () => <ChatBoxNav />);
