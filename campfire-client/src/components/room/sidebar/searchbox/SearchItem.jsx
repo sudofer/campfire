e7e39b8071f0dd0864406d51a88988b0892901ad
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-08-03 18:15:10 -0400

    Co-authored-by: Vivian Cheung <vvncheung@users.noreply.github.com>

--- campfire-client/src/components/room/sidebar/searchbox/SearchItem.jsx
+++ campfire-client/src/components/room/sidebar/searchbox/SearchItem.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import "./SearchItem.css";
 export default function SearchItem({ result, submitPlayListItem }) {
+  console.log(result);
+  const style = {
+    width: "90px",
+    height: "45px",
+  };
   return (
-    <li>
-      <h5>{result.link}</h5>
+    <li className="SearchItemBox">
+      <h5>{result.title}</h5>
+      <img style={style} src={result.thumbnails.default.url}></img>
       <button
         onClick={() => {
           submitPlayListItem(result);
