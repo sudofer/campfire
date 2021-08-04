203462759c3812fc95cb66591becba0ddde8abce
Vivian Cheung <hi@viviancheung.ca>
2021-08-04 04:52:56 -0400

    complete chat functionality in same room across multiple websocket connections, baby styling

--- campfire-client/src/components/room/sidebar/chatbox/MessageInput.jsx
+++ campfire-client/src/components/room/sidebar/chatbox/MessageInput.jsx
@@ -1,5 +1,44 @@
 import React from "react";
+import { TextField } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button'
+// import Fab from '@material-ui/core/Fab';
+// import EditIcon from '@material-ui/icons/Edit';
 
-export default function MessageInput() {
-  return <></>;
+
+export default function MessageInput({ message, setMessage, sendMessage }) {
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
+  }));
+
+  const classes = useStyles();
+  return (
+  <>
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="standard-basic" label="Type your message..." 
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+        onKeyPress={(event) => event.key === 'Enter'? sendMessage(event) : null}
+      />
+      <Button className="primary" onClick={(event) => sendMessage(event)}>Send</Button>
+      {/* <Fab size="small" color="primary" aria-label="edit" className={classes.margin}
+        onClick={(event) => sendMessage(event)}>
+          <EditIcon />
+        </Fab> */}
+    </form>
+  </>
+  );
 }
+
--- campfire-client/src/components/room/sidebar/chatbox/index.jsx
+++ campfire-client/src/components/room/sidebar/chatbox/index.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
+import MessageInput from './MessageInput'
+import MessageList from './MessageList'
+import './Message.css'
 
 let socket;
 
@@ -21,6 +24,7 @@ export default function Chatbox() {
     const name = urlParams.get('name');
     const url = urlParams.get('url');
 
+    // using dedicated chat socket temporarily - need to replace with room socket
     socket = io(ENDPOINT);
 
     setName(name);
@@ -55,12 +59,15 @@ export default function Chatbox() {
 
   return (
   <div className="outerContainer">
-    <div className="container">
-    <input 
-      value={message} 
-      onChange={(event) => setMessage(event.target.value)}
-      onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null}
-    />
+    <div className="insideContainer">
+      <MessageList 
+        messages={messages}
+        name={name}
+      />
+      <MessageInput 
+        message={message}
+        setMessage={setMessage}
+        sendMessage={sendMessage}/>
     </div>
   </div>
   )
