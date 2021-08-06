a972f4cf6c35f31f4b3adccd9f0d93a45cd99ad6
Vivian Cheung <hi@viviancheung.ca>
2021-08-06 03:15:18 -0400

    front page styling

--- campfire-client/src/components/home/Home.jsx
+++ campfire-client/src/components/home/Home.jsx
@@ -5,8 +5,14 @@ import { makeStyles } from '@material-ui/core/styles';
 import {Container} from '@material-ui/core'; // this must be imported last
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import './Home.css';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ScreenShareIcon from '@material-ui/icons/ScreenShare';
 import theme from "../../theme";
+import './Home.css';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export default function Home() {
   const [url, setUrl] = useState("");
@@ -40,7 +46,6 @@ export default function Home() {
   }
 
   // front page grid styling
-
   const useStyles = makeStyles(theme => ({
     root: {
       padding: theme.spacing(12, 4),
@@ -51,12 +56,11 @@ export default function Home() {
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
-      border: '1px solid black',
+      border: '0px solid black',
       borderRadius: '5px',
       textAlign: 'center',
-    },
-    icon: {
-      padding: theme.spacing(2, 0),
+      backgroundColor: 'rgba(0,0,0,0.8)',
+      color: 'white',
     },
     title: {
       padding: theme.spacing(2),
@@ -64,13 +68,35 @@ export default function Home() {
     featureList: {
       padding: theme.spacing(2),
     },
+    Button: {
+      size: 'large',
+    },
+    icon: {
+      padding: theme.spacing(2,0),
+      size: 'large',
+      color: '#fff',
+    },
   }));
+
+  // dialog pop up boxes set to dark mode
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const darkMode = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
 
   const classes = useStyles();
 
   return (
     <>
 
+    {/* video background */}
+    <div id="frontPageContainer">
     <div class="video-background">
       <div class="video-foreground">
         <iframe title="intentionally_blank" aria-hidden="true" src="https://www.youtube.com/embed/NUKKzdVy0EI?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=NUKKzdVy0EI&mute=1" allow='autoplay' volume='0' frameborder="0" allowfullscreen/>
@@ -79,143 +105,143 @@ export default function Home() {
 
     {/* front page grid */}
 
-    <div>
-      Join your friends around the campfire
-      
-    </div>
-    <Container component="section" maxWidth="lg" className={classes.root}>
-      <Grid container spacing={3} alignItems="stretch">
-        <Grid item xs={12} sm={4}>
-          <div className={classes.card}>
-            {/* <EmojiPeopleIcon
-              color="primary"
-              fontSize="large"
-              className={classes.icon}
-            /> */}
-            <Typography variant="h5" component="h3" className={classes.title}>
-              blahblahblah
-            </Typography>
-            <Typography className={classes.featureList}>
-              lorem ipsum something something
-            </Typography>
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <div className={classes.card}>
-            {/* <FastfoodIcon
-              color="primary"
-              fontSize="large"
-              className={classes.icon}
-            /> */}
-            <Typography variant="h5" component="h3" className={classes.title}>
-            blahblahblah
-            </Typography>
-            <Typography className={classes.featureList}>
-            lorem ipsum something something
-            </Typography>
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <div className={classes.card}>
-            {/* <LocationCityIcon
-              color="primary"
-              fontSize="large"
-              className={classes.icon}
-            /> */}
-            <Typography variant="h5" component="h3" className={classes.title}>
-              blahblahblah
-            </Typography>
-            <Typography className={classes.featureList}>
-            lorem ipsum something something
-            </Typography>
-          </div>
-        </Grid>
-      </Grid>
-    </Container>
+    <div id="frontPageBody">
 
+      <div className="pageTitle">
+        <h1>
+          Join your friends around the campfire.  
+        </h1>
+      </div>
 
-    {/* Create Room button */}
-    <div className="create-room">
-      <Button color="secondary" onClick={() => handleClick('create')}>Create room</Button>
-      <Dialog open={openType === 'create'} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Create Room</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Please enter your name and click join!
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name-create"
-            label="Name"
-            type="text"
-            name="name"
-            value={name}
-            onChange={event => setName(event.target.value)}
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="url-create"
-            label="Url"
-            value={url}
-            type="text"
-            name="url"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleSubmit} color="primary">
-            Join
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+      <Container component="section" maxWidth="lg" className={classes.root}>
+        <Grid container spacing={3} alignItems="stretch">
+          <Grid item xs={12} sm={4}>
+            <div className={classes.card}>
+              <Typography variant="h4" component="h4" className={classes.title}>
+                Start a room
+              </Typography>
+              <ScreenShareIcon style={{ fontSize: 80 }} className={classes.icon}/>
+              <Typography className={classes.featureList}>
+                Create a new campfire for you and your friends, then share the link
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <div className={classes.card}>
+              <Typography variant="h4" component="h4" className={classes.title}>
+              Curate
+              </Typography>
+              <SubscriptionsOutlinedIcon style={{ fontSize: 80 }} />
+              <Typography className={classes.featureList}>
+              Add videos to your playlist and start watching
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <div className={classes.card}>
+              <Typography variant="h4" component="h4" className={classes.title}>
+                Stay warm
+              </Typography>
+              <FavoriteIcon style={{ fontSize: 80 }} />
+              <Typography className={classes.featureList}>
+                Enjoy while chatting with your friends
+              </Typography>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
 
-    {/* Join Room button */}
-    <div className="join-room">
-    <Button color="secondary" onClick={() => handleClick('join')}>Join room</Button>
-      <Dialog open={openType === "join"} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Join Room</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Please enter your name/url and click join!
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name-join"
-            label="Name"
-            type="text"
-            name="name"
-            value={name}
-            onChange={event => setName(event.target.value)}
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="url-join"
-            label="Url"
-            type="text"
-            value={url}
-            onChange={event => setUrl(event.target.value)}
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleSubmit} color="primary">
-            Join
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <ThemeProvider theme={darkMode}> 
+      <CssBaseline/>
+
+      <div className="roomButtons">  
+        {/* Create Room button */}
+        <div className="create-room">
+          <Button size="large" onClick={() => handleClick('create')}>Create room</Button>
+          <Dialog open={openType === 'create'} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Create Room</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Please enter your name to create your room!
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name-create"
+                label="Name"
+                type="text"
+                name="name"
+                value={name}
+                onChange={event => setName(event.target.value)}
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="url-create"
+                label="Room URL"
+                value={url}
+                type="text"
+                name="url"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>
+                Cancel
+              </Button>
+              <Button onClick={handleSubmit}>
+                Join
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+
+        {/* Join Room button */}
+        <div className="join-room">
+        <Button size="large" onClick={() => handleClick('join')}>Join room</Button>
+          <Dialog open={openType === "join"} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Join Room</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Please enter your name and room url to join!
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name-join"
+                label="Name"
+                type="text"
+                name="name"
+                value={name}
+                onChange={event => setName(event.target.value)}
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="url-join"
+                label="Room URL"
+                type="text"
+                value={url}
+                onChange={event => setUrl(event.target.value)}
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>
+                Cancel
+              </Button>
+              <Button onClick={handleSubmit}>
+                Join
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      </div>
+      </ThemeProvider>
+      <CssBaseline/>
+      </div>
     </div>
   </>
   )
